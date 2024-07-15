2ec5e70e663b6b434aaa9d66a72f89cbda71bd90
Jorge Benavente <jorge.benavente2003@gmail.com>
2024-07-15 21:00:24 +0200

    Remove old nvim config

--- nvim/lua/plugins/delimitMate.lua
+++
@@ -1,2 +0,0 @@
-vim.g.delimitMate_expand_cr = 0
-vim.g.delimitMate_expand_space = 1
--- nvim/lua/plugins/toggleterm.lua
+++
@@ -1,5 +0,0 @@
-require('toggleterm').setup{}
-
-vim.keymap.set('n', '<leader>tt', ':ToggleTerm direction=horizontal<CR>', {desc = 'Open terminal'})
-vim.keymap.set('t', '<leader>tt', '<C-\\><C-n>:ToggleTerm direction=horizontal<CR>', {desc = 'Open terminal'})
-vim.keymap.set('n', '<leader>tv', ':ToggleTerm direction=vertical<CR>', {desc = 'Open vertical terminal'})
